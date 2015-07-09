3b50f3163edc60bcffbc782ef44eb624a1f5372d
akwasiowusu28 <akwasiowusu28@gmail.com>
2015-07-08 23:12:13 -0500

    Initial commit

---
+++ app/src/main/java/com/republic/ui/support/NavigationHelper.java
@@ -0,0 +1,43 @@
+package com.republic.ui.support;
+
+import com.republic.ui.R;
+
+/** *
+ * Created by Akwasi Owusu on 7/6/15.
+ */
+public class NavigationHelper {
+
+    /**
+     * Defines the main navigation items on the main page. Add to this if you
+     * want to add another item on the navigation page
+     *
+     * @return String ids of the navigation items
+     */
+    public static Integer[] getMainPageNavigationItems() {
+        return new Integer[]{R.string.fraud, R.string.bribery, R.string.abuse, R.string.waste,
+                R.string.extortion, R.string.embezzlement, R.string.favoritism};
+    }
+
+    /**
+     * Defines the main navigation item descriptions on the main page. Add to this
+     * if you want to add another item on the navigation page
+     *
+     * @return String ids of the navigation items
+     */
+    public static Integer[] getMainPageDescriptions() {
+        return new Integer[]{R.string.frauddescription, R.string.briberydescription, R.string.abusedescription,
+                R.string.wastedescription, R.string.extortiondescription, R.string.embezzlementdescription,
+                R.string.favoritismdescription};
+    }
+
+    /**
+     * Returns the navigation icons used in the main page
+     *
+     * @return Ids for Icons used in the main page
+     */
+    public static Integer[] getMainPageIcons() {
+        return new Integer[]{R.drawable.fraud, R.drawable.bribery, R.drawable.abuse, R.drawable.waste,
+                R.drawable.extortion, R.drawable.embezzlement, R.drawable.favoritism};
+    }
+
+}
