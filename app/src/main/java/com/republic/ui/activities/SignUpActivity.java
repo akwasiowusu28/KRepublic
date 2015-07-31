4b079783215898b12f034762edccdef410d6c212
akwasiowusu28 <akwasiowusu28@gmail.com>
2015-07-31 00:50:14 -0500

    New UI ideas for sign up, and login

--- app/src/main/java/com/republic/ui/activities/SignUpActivity.java
+++ app/src/main/java/com/republic/ui/activities/SignUpActivity.java
@@ -1,11 +1,11 @@
 package com.republic.ui.activities;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,7 +18,7 @@ import com.republic.support.RepublicFactory;
 import com.republic.ui.R;
 import com.republic.ui.support.Utils;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity {
 
     private UserController userController;
     private String phoneNumber;
